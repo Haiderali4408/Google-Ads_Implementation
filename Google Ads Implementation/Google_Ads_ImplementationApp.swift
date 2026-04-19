3c92ce0844383d58cdacd330150707c87d1570cb
Haider Ali <haider.ali4408@gmail.com>
2026-04-19 10:23:38 +0500

    Initial Commit

---
+++ Google Ads Implementation/Google_Ads_ImplementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_Ads_ImplementationApp.swift
+//  Google Ads Implementation
+//
+//  Created by Haider's Macbook on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Google_Ads_ImplementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
